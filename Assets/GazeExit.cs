a834f8d483e945e3846c034a2bc43256806dbe3e
x0r0ch1 <66729953+x0r0ch1@users.noreply.github.com>
2021-10-23 18:11:27 +0700

    Initial commit

---
+++ Assets/GazeExit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeExit : MonoBehaviour
+{
+    public GameObject InfoObject;
+    private bool Show = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitApp()
+    {
+        if (Show == true)
+        {
+            Application.Quit();
+        }
+    }
+}
